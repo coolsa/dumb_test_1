7708923d02a3cf302d8b64c6708ca9e17d7cd6db
coolsa <noob.cloud@gmail.com>
2017-05-01 16:49:47 -0600

    made things more modular, now functions define cube creation! cool beans.

--- script/main.js
+++ script/main.js
@@ -28,31 +28,45 @@ function ($){
 	});
 	iso = Crafty.isometric.size(32);
 	var z=0;
-	for(var x=0;x<16;x++){
-		for(var y=0;y<32;y++){
+	for(var x=0;x<5;x++){
+		for(var y=0;y<5;y++){
 			for(var z=0;z<3;z++){
-				var tile = Crafty.e("2D", "Canvas", "Mouse", "impulse")
-				.attr('z',y+z*32)//
-				.areaMap(16,0, 0,8, 0,24, 16,32, 32,24, 32,8) //series of points defining area
-				.bind("drawFace",function(dir){
-					drawWith(this._x,this._y,this._z,iso,this,dir);
-					drawWith(this._x,this._y,this._z,iso,this,"empty");
-				})
-				.bind("Click",function(e){
-					console.log(e.button);
-					this.destroy();
-				}).bind("MouseOver",function(e){
-					this._children[2].sprite("select");
-				}).bind("MouseOut",function(e){
-					this._children[2].sprite("empty");
-				});
-				iso.place(x,y,z*2,tile);
+				var tile = newCube(iso,-y+z*32);
+				drawAt(x,y,z,tile,iso);
 				if(z==0)tile.sprite("chain").trigger("drawFace","pos_z_norm");
-				if(z==1)tile.trigger("drawFace","pos_x_norm");
+				if(z==1)tile.sprite("impulse").trigger("drawFace","pos_x_norm");
 				if(z==2)tile.sprite("repeating").trigger("drawFace","neg_y_cond");
 			}
 		}
 	}
+	function newCube(iso,layer){//create a new cube with clickable uses! made to modify
+		return Crafty.e("2D", "Canvas", "Mouse", "empty")
+		.attr('z',layer)//
+		.areaMap(16,0, 0,8, 0,24, 16,32, 32,24, 32,8) //series of points defining area
+		.bind("drawFace",function(dir){
+			drawOver(this._x,this._y,this._z,iso,this,dir);
+			drawOver(this._x,this._y,this._z,iso,this,"empty");
+		})
+		.bind("Click",function(e){
+			console.log(e.button);
+			this.destroy();
+		}).bind("MouseOver",function(e){
+			this._children[2].sprite("select");
+		}).bind("MouseOut",function(e){
+			this._children[2].sprite("empty");
+		});
+	}
+	function drawAt(x,y,z,tile,iso){
+		yPos=x-y;
+		iso.place(x-Math.ceil(yPos/2),yPos,z*2,tile);
+	}
+	function drawOver(x,y,z,iso,parent,sprite){
+		px = iso.px2pos(x,y);
+		child=parent._children.length;
+		parent.attach(Crafty.e("2D","Canvas",sprite).attr('z',z));
+		iso.place(px.x,px.y,0,parent._children[child]);
+	}
+	//the below allows movement with the thingy. copy paste from example, #plagerism
 	Crafty.addEvent(this, Crafty.stage.elem, "mousedown", function(e) {
 		if(e.button > 1) return;
 		var base = {x: e.clientX, y: e.clientY};
@@ -70,14 +84,4 @@ function ($){
 			Crafty.removeEvent(this, Crafty.stage.elem, "mousemove", scroll);
 		});
 	});
-	function drawWith(x,y,z,iso,parent,sprite){
-		px = iso.px2pos(x,y);
-		child=parent._children.length;
-		parent.attach(Crafty.e("2D","Canvas",sprite).attr('z',z));
-		iso.place(px.x,px.y,0,parent._children[child]);
-	}
-	// function placeAt(x,y,z,obj,iso){
-	// 	px = iso.px2pos(x,y);
-	// 	iso.place(px.x,px.y,z,obj);
-	// }
 });
